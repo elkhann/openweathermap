95247659f106e08c70974e434eb49400135c9007
Emil Khann <emilkhann@gmail.com>
2019-10-28 17:22:39 +0300

    day 6 start geo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Header from './components/Header';
 import OpenWeather from './containers/OpenWeather';
 
 const App = () => {
+  navigator.geolocation.getCurrentPosition(function(location) {
+    console.log(location.coords.latitude);
+    console.log(location.coords.longitude);
+    console.log(location.coords.accuracy);
+  });
   return (
     <div className='App'>
       <Header />
